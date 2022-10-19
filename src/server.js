009c54bd37dc5b1aa80eb36dfc94a6ec76c756a2
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-10-19 16:21:47 -0300

    created token verification

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
-const server = express()
-const PORT = process.env.PORT
+const server = express();
+const PORT = process.env.PORT;
 
 server
     .use(express.json())
-    .use(cors())
+    .use(cors());
 
 server.get("/status", (req, res) => {
     res.send("server it's on")
-})
+});
 
-server.listen(PORT, () => {console.log(`Server listen on PORT ${PORT}`)})
+server.listen(PORT, () => {console.log(`Server listen on PORT ${PORT}`)});
