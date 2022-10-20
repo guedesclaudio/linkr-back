16e633037b284712a499214b6e6355c5afa82bc7
gacdutra <ga.acdutra@gmail.com>
2022-10-20 18:47:49 -0300

    feat: added user signin routes, repositories and controllers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createNewUser } from "../controllers/users.controllers.js";
-import { validateNewUser } from "../middlewares/users.middlewares.js";
+import { createNewUser, postLogin } from "../controllers/users.controllers.js";
+import {
+  validateNewUser,
+  validateLogin,
+} from "../middlewares/users.middlewares.js";
 
 const router = express.Router();
 
 router.post("/signup", validateNewUser, createNewUser);
+router.post("/signin", validateLogin, postLogin);
 
 export default router;
