fa9be9c33bf2b9cf2cb7b352ad19101b8db5aa4c
gacdutra <ga.acdutra@gmail.com>
2022-10-21 12:47:21 -0300

    fix: fixed semantic errors

--- src/middlewares/token.middleware.js
+++ src/middlewares/token.middleware.js
@@ -1,31 +1,32 @@
 import connection from "../database/database.js";
 
-async function verifyToken (req, res, next) {
-    const authorization = req.headers.authorization;
-    const token = authorization?.replace("Bearer ", "");
+async function verifyToken(req, res, next) {
+  const authorization = req.headers.authorization;
+  const token = authorization?.replace("Bearer ", "");
 
-    try {
-        if (token !== null && token !== undefined) {
-            const findSession = connection.query(`
-                SELECT * FROM sessions WHERE token = '$1';
-            `, [token]);
-    
-            if (findSession.rows.length > 0) {
-                const findUser = connection.query(`
-                    SELECT * FROM users WHERE id = $1;
-                `, [session.rows[0].user_id]);
-
-                const user = findUser.rows[0];
-                delete user.password;
-                res.locals.user = user;
-                return next;
-            }
-        }
-        return res.sendStatus(401);
-
-    } catch (error) {
-        return res.sendStatus(500);
+  try {
+    if (token !== null && token !== undefined) {
+      const findSession = await connection.query(
+        `SELECT * FROM sessions WHERE token = $1;`,
+        [token]
+      );
+      if (findSession.rows.length > 0) {
+        const findUser = await connection.query(
+          `SELECT * FROM users WHERE id = $1;`,
+          [findSession.rows[0].user_id]
+        );
+        const user = findUser.rows[0];
+        delete user.password;
+        res.locals.user = user;
+        res.locals.token = token;
+        return next();
+      }
     }
+    return res.sendStatus(401);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
 }
 
 export { verifyToken };
