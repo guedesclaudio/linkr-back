870d76414f457717cd4d7243b858144fb0dfa14e
gacdutra <ga.acdutra@gmail.com>
2022-10-21 12:49:31 -0300

    fix: deleted local database information

--- src/database/database.js
+++ src/database/database.js
@@ -4,21 +4,13 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const databaseConfig = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// };
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
-// const connection = new Pool(databaseConfig);
-
-const connection = new Pool({
-  user: "postgres",
-  password: "124816",
-  host: "localhost",
-  port: 5432,
-  database: "linkr",
-});
+const connection = new Pool(databaseConfig);
 
 export default connection;
