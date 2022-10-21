edec296e5ac0c1eda7e7a507b189891ec9f6ccb1
gacdutra <ga.acdutra@gmail.com>
2022-10-21 12:48:06 -0300

    feat: route that checks if session is valid

--- src/database/database.js
+++ src/database/database.js
@@ -4,13 +4,21 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const databaseConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+// const databaseConfig = {
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// };
 
-const connection = new Pool(databaseConfig);
+// const connection = new Pool(databaseConfig);
+
+const connection = new Pool({
+  user: "postgres",
+  password: "124816",
+  host: "localhost",
+  port: 5432,
+  database: "linkr",
+});
 
 export default connection;
