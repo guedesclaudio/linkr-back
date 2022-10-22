170216a58db02b256602f96ee0a6699d0dec9f87
Israel-Felipe <israelfelipet@gmail.com>
2022-10-22 17:34:07 -0300

    insert hashtags in table hashtags and middle hashtags_posts

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,16 +1,50 @@
 import * as postsRepository from "../repositories/posts.repository.js";
+import findHashtags from "find-hashtags";
 
-async function insertPost (req, res) {
-    const user = res.locals.user;
-    const { post_url, body } = req.body;
+const insertPost = async (req, res) => {
+  const user = res.locals.user;
+  const { post_url, body } = req.body;
 
-    try {
-        await postsRepository.insertPostInDB(user.id, post_url, body);
-        return res.sendStatus(201);
+  try {
+    const insertPost = await postsRepository.insertPostInDB(
+      user.id,
+      post_url,
+      body
+    );
 
-    } catch (error) {
-        return res.sendStatus(500);
+    const postId = insertPost.rows[0].id;
+    console.log(postId);
+
+    const hashtags = findHashtags(body);
+    const hashtagsId = [];
+
+    if (hashtags.length !== 0) {
+      for (let i = 0; i < hashtags.length; i++) {
+        const hashtagFound = (await postsRepository.selectHashtag(hashtags[i]))
+          .rows[0];
+
+        if (hashtagFound) {
+          hashtagsId.push(hashtagFound.id);
+          continue;
+        }
+
+        let hashtag = await postsRepository.insertIntoHashtags(hashtags[i]);
+        hashtagsId.push(hashtag.rows[0].id);
+      }
+
+      for (let i = 0; i < hashtagsId.length; i++) {
+        postsRepository.insertIntoMiddleHashtagsPosts({
+          post_id: postId,
+          hashtag_id: hashtagsId[i],
+        });
+      }
     }
-}
+
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+};
 
 export { insertPost };
--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -1,12 +1,38 @@
 import connection from "../database/database.js";
 
-function insertPostInDB (userId, publishedUrl, publishedBody) {
-    return connection.query(`
+function insertPostInDB(userId, publishedUrl, publishedBody) {
+  return connection.query(
+    `
         INSERT INTO
             posts (user_id, post_url, body)
         VALUES
-            ($1, $2, $3);
-    `, [userId, `${publishedUrl}`, `${publishedBody}`]);
+            ($1, $2, $3) RETURNING id;
+    `,
+    [userId, `${publishedUrl}`, `${publishedBody}`]
+  );
 }
 
-export { insertPostInDB };
+const selectHashtag = (hashtag) => {
+  return connection.query(`SELECT * FROM hashtags WHERE name = $1;`, [hashtag]);
+};
+
+const insertIntoHashtags = (hashtag) => {
+  return connection.query(
+    "INSERT INTO hashtags (name) VALUES ($1) RETURNING id;",
+    [hashtag]
+  );
+};
+
+const insertIntoMiddleHashtagsPosts = ({ post_id, hashtag_id }) => {
+  return connection.query(
+    `INSERT INTO hashtags_posts (post_id, hashtag_id) VALUES ($1, $2);`,
+    [post_id, hashtag_id]
+  );
+};
+
+export {
+  insertPostInDB,
+  selectHashtag,
+  insertIntoHashtags,
+  insertIntoMiddleHashtagsPosts,
+};
