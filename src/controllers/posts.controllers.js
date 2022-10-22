848aa6a79244a55b9096ab83c6ddb695448ffb03
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-10-22 19:10:25 -0300

    edit post created

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -13,4 +13,29 @@ async function insertPost (req, res) {
     }
 }
 
-export { insertPost };
+async function editPost (req, res) {
+    const user = res.locals.user;
+    const post_id = req.headers.postid;
+    const { body } = req.body;
+
+    try {
+        const postBelongsToUser = await postsRepository.checkIfPostBelongsToUser(user.id, post_id);
+
+        if (postBelongsToUser) {
+            try {
+                await postsRepository.editPostInDB(post_id, body);
+                return res.sendStatus(204);
+
+            } catch (error) {
+                return res.sendStatus(500);
+            }
+        }
+
+        return res.sendStatus(401);
+        
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+}
+
+export { insertPost, editPost };
--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -9,4 +9,24 @@ function insertPostInDB (userId, publishedUrl, publishedBody) {
     `, [userId, `${publishedUrl}`, `${publishedBody}`]);
 }
 
-export { insertPostInDB };
+async function checkIfPostBelongsToUser (user_id, post_id) {
+    const post = await connection.query(`
+        SELECT * FROM posts WHERE id = $1 AND user_id = $2;
+    `, [post_id, user_id]);
+
+    if (post.rows.length > 0) {
+        return true;
+    }
+
+    return false;
+}
+
+function editPostInDB (post_id, body) {
+    return connection.query(`
+        UPDATE posts
+        SET body = $1
+        WHERE id = $2;
+    `, [body, post_id]);
+}
+
+export { insertPostInDB, checkIfPostBelongsToUser, editPostInDB };
