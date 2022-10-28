31f514c16df1e30bef14a0b71ccf89123d096edc
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-10-28 11:51:00 -0300

    1. added getComments; 2. added getLabel with getIfIsPostsAuthor and getIfIsFollowing

--- src/controllers/comments.controllers.js
+++ src/controllers/comments.controllers.js
@@ -20,7 +20,49 @@ async function insertComment(req, res) {
 }
 
 async function getComments(req, res) {
-    
+  const post_id = req.params.postId;
+
+  try {
+    const comments = await commentsRepository.getCommentsInDB(post_id);
+    return res.status(200).send(comments);
+
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
 }
 
-export { insertComment, getComments };
+async function getLabel(req, res) {
+  const user = res.locals.user;
+  const post_id = req.params.postId;
+  const author_id = req.params.authorId;
+  const comment_id = req.params.commentId;
+
+  try {
+    const responsePostsAuthor = await commentsRepository.getPostsAuthor(post_id, author_id, comment_id);
+    const responseFollowing = await commentsRepository.getFollowAuthor(author_id, user.id, comment_id);
+
+    if (responsePostsAuthor.comment_author === responsePostsAuthor.post_author) {
+      return res.status(200).send({ isThePostsAuthor: true });
+
+    } else {
+      if (responseFollowing.length > 0) {
+        return res.status(200).send({ 
+          isThePostsAuthor: false,
+          isFollowing: true
+        });
+      }
+
+      return res.status(200).send({ 
+        isThePostsAuthor: false,
+        isFollowing: false
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export { insertComment, getComments, getLabel };
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middlewares/token.middleware.js";
 import { verifyNewCommentSchema } from "../schemas/schemas.js";
-import { insertComment } from "../controllers/comments.controllers.js";
+import { insertComment, getComments, getLabel } from "../controllers/comments.controllers.js";
 
 const router = express.Router();
 
 router.post("/comments", verifyToken, verifyNewCommentSchema, insertComment);
+router.get("/comments/:postId", verifyToken, getComments);
+router.get("/comments/:postId/:authorId/:commentId", verifyToken, getLabel);
 
 export default router;
